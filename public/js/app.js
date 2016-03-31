e1e4f05589ba4d1add3ce4af143c4394b70ee166
danielxji6 <danielxji6@gmail.com>
2016-03-30 22:20:33 -0700

    add new texture; make mouseleave event

--- public/js/app.js
+++ public/js/app.js
@@ -1,8 +1,19 @@
+console.log("app.js loaded");
+
 var box = document.querySelector('a-box');
 box.addEventListener('mouseenter', function () {
   box.setAttribute('scale', {
-    x: 4,
+    x: 2,
     y: 1,
-    z: 6
+    z: 1
+  });
+  box.setAttribute('src', '#iron_texture');
+});
+
+box.addEventListener('mouseleave', function () {
+  box.setAttribute('scale', {
+    x: 2,
+    y: 1,
+    z: 3
   });
 });
--- server.js
+++ server.js
@@ -1,15 +1,19 @@
 var express = require('express'),
-    app = express(),
-    bodyParser = require('body-parser');
+  app = express(),
+  bodyParser = require('body-parser');
+var https = require('https');
+var http = require('http');
 
 // configure bodyParser (for receiving form data)
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 
 // serve static files from public folder
 app.use(express.static(__dirname + '/public'));
 
 
 // listen on port 3000
-app.listen(process.env.PORT || 3000, function () {
+app.listen(process.env.PORT || 3000, function() {
   console.log('Express server is running on http://localhost:3000/');
 });
